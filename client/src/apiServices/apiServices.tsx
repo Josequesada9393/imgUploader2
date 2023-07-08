618431d63455be4ebc9c4d47d852f58239eb6430
Jose Alberto Gomez <josequesada9393@gmail.com>
2023-07-08 15:24:26 -0700

    new api services

--- client/src/apiServices/apiServices.tsx
+++ client/src/apiServices/apiServices.tsx
@@ -1,7 +1,8 @@
 
 
 // const baseURL = 'http://localhost:3000/'
-// const serverURL = 'https://image-uploader-server-woad.vercel.app/';
+// const serverURL = 'https://image-uploader-server-woad.vercel.app/';J
+const baseURL = 'https://serverimage-uqp6.onrender.com/'
 
 export async function uploadFile<File extends BodyInit | null | undefined>(my_file: File): Promise<string> {
   const response = await fetch(`${baseURL}addImage`, {
